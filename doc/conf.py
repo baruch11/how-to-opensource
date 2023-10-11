133ca656b0216182b93eb539bbdbb36093e1aabb
Charles-Henri Prat <chprat@quantmetry.com>
2023-10-11 15:06:49 +0200

    Bump version: 0.0.4 → 0.0.5

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Grégoire Martinon, Vianney Taquet, Damien Hervault'
 author = 'Grégoire Martinon, Vianney Taquet, Damien Hervault'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.4'
+release = '0.0.5'
 
 
 # -- General configuration ---------------------------------------------------
